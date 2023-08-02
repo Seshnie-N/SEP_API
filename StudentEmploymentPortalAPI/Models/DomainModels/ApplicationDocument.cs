00e9bb51bf688c7212ba5a47ded500bd0764d945
seshnie <seshnie.naidoo26@gmail.com>
2023-08-02 11:45:19 +0200

    application document, student application relationship

--- StudentEmploymentPortalAPI/Models/DomainModels/ApplicationDocument.cs
+++ StudentEmploymentPortalAPI/Models/DomainModels/ApplicationDocument.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudentEmploymentPortalAPI.Models.DomainModels
 {
     public class ApplicationDocument
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string FileType { get; set; }
@@ -15,8 +14,9 @@ namespace StudentEmploymentPortalAPI.Models.DomainModels
         public string Description { get; set; }
         public DateTime? CreatedDate { get; set; }
         public string FilePath { get; set; }
+        [ForeignKey(nameof(Application))]
        public Guid StudentApplicationId { get; set; }
-        /* [ValidateNever]
-        public StudentApplication Application { get; set; }*/
+        [ValidateNever]
+        public StudentApplication Application { get; set; }
     }
 }
